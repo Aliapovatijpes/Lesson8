055e0b3129b942e2b0ffcb05e17298916e5eb95b
Aliapovatijpes <84346624+Aliapovatijpes@users.noreply.github.com>
2022-08-10 18:21:30 +0300

    changes in env variables

--- user_data.sh
+++ user_data.sh
@@ -12,10 +12,6 @@ echo \
 $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-export MYSQL_ROOT_PASSWORD=rootpass
-export PMA_HOST=mysql-service
-export PMA_PORT=3306
-export MYSQL_ROOT_PASSWORD=rootpass
 mkdir -p /home/admin
 cd /home/admin
 touch compose.yaml
@@ -25,6 +21,8 @@ services:
         image: mysql:8.0.30-debian
         networks:
             - lamp
+        environment:
+            - MYSQL_ROOT_PASSWORD=rootpass
         expose:
             - 3306
         restart: always
@@ -34,6 +32,9 @@ services:
             - lamp
         ports:
             - "80:80"
+        environment:
+            - PMA_HOST=mysql
+            - PMA_PORT=3306
         depends_on:
             - mysql
         restart: always
